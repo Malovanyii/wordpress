1022c2632d515d774fa592dcab583934bc500250
malovaniyy@gmail.com <malovaniyy@gmail.com>
2023-05-22 01:49:36 +0300

    task2

--- js/task1.js
+++ js/task1.js
@@ -1,19 +1,35 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели ?', '');
 
 const personaMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов ?', ''),
-    b = prompt('На сколько оцените его ?', ''),
-    с = prompt('Один из последних просмотренных фильмов ?', ''),
-    d = prompt('На сколько оцените его ?', '');
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов ?', ''),
+		b = prompt('На сколько оцените его ?', '');
 
-personaMovieDB.movies[a] = b;
-personaMovieDB.movies[c] = d;
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personaMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+	personaMovieDB.movies[a] = b;
+}
 
 console.log(personaMovieDB);
+
+if (personaMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personaMovieDB.movies > 10 && personaMovieDB.movies < 30) {
+	console.log('Вы класический зритель');
+} else if (personaMovieDB.movies > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('error');
+}
